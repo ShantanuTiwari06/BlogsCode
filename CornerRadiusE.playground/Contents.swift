0065a12f54a85a50b9c93f2ed8c6463ca480659f
Shantanu Tiwari <shantanut710@gmail.com>
2022-05-14 13:30:59 +0530

    Initial Commit

---
+++ CornerRadiusE.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+extension UIView {
+    func provideCornerRadius(corners: CACornerMask, radius: CGFloat) {
+        self.layer.maskedCorners = corners
+        self.layer.cornerRadius = radius
+    }
+}
